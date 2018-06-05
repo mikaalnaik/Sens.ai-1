9aaecbb825c5f164f373cb49d09c389264db30d4
Sebastian <svnineseven@gmail.com>
2018-06-05 19:03:18 -0400

    implemented functionality to make query from front end that will be processed and returned as analyzed statistial data

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fetchRedditPosts = require('./media_modules/reddit-api')
 const fetchTwitterPosts = require('./media_modules/twitter-api')
+const statsCalculator = require('./statistics/Statistics.js')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -12,10 +13,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-app.get("/results/:id", async (req, res) => {
 
+// this route will respond with the analyzed posts from reddit and twitter
+app.get("/results/:id", async (req, res) => {
     let redditPosts = await fetchRedditPosts(req.params.id)
     let twitterPosts = await fetchTwitterPosts(req.params.id)
+    let allPosts = redditPosts.concat(twitterPosts)
+    allPosts = await Promise.all(allPosts)
+    let stats = await statsCalculator(allPosts)
+    console.log(stats)
 
-    res.json({redditPosts: redditPosts, twitterPosts: twitterPosts})
 })
---
+++ server/statistics/AI_Modules/SentimentAnalyzer.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+var toneAnalyzer = new ToneAnalyzerV3(
+  {
+    "version": "2017-09-21",
+    "url": "https://gateway.watsonplatform.net/tone-analyzer/api",
+    "username": process.env.IBM_USER,
+    "password": process.env.IBM_PASSWORD
+  });
+
+
+const sentimentAnalyzer = function(post) {
+  return new Promise(function(resolve, reject) {
+
+    post.tones = [];
+
+    let toneParams = { // toneParams is the required input for the toneAnalyzer API
+      'tone_input': { 'text': post.content },
+      'content_type': 'application/json'
+    }
+
+    toneAnalyzer.tone(toneParams, function (error, analysis) {
+      const tones = analysis.document_tone.tones
+
+      if (error) {
+        console.log('error',error);
+        return reject(error)
+      } else {
+        if (tones.length > 0) { // eluding a scenario where content has no analyzed sentimentss
+          tones.forEach( tone => {
+            if (tone.tone_id === 'sadness' || tone.tone_id === 'anger' || tone.tone_id === 'fear' || tone.tone_id === 'tentative' || tone.tone_id === 'joy' || tone.tone_id === 'confident') {
+              post.tones.push({tone: tone.tone_name, confidence: tone.score})
+            }
+          })
+        }
+
+        return resolve(post);
+      }
+    })
+  })
+};
+
+module.exports = sentimentAnalyzer
