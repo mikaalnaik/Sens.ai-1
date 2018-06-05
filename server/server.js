e5283ed1017f7465db5809fdf8e5a6ffea84fdcd
Sebastian <svnineseven@gmail.com>
2018-06-05 14:09:11 -0400

    refactored some code

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
 require('dotenv').config()
 
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 const express = require("express");
 const bodyParser = require("body-parser");
-let Twit = require('twit');
-
-const app = express();
-app.use(bodyParser.urlencoded({extended: true}));
+const Twit = require('twit');
+const fetchRedditContent = require('./redditAPI')
 
 const PORT = process.env.PORT || 3001;
-console.log(process.env.SECRETCONSUMERKEY);
-
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'))
-.listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
+
+
 
 // Twitter API
 var T = new Twit({
@@ -23,44 +23,24 @@ access_token_secret: process.env.SECRETACCESSTOKENSECRET,
 timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 strictSSL:            true,     // optional - requires SSL certificates to be valid.
 })
-console.log(T.config.consumer_key);
-
-app.get("/api/hello", (req, res) => {
-
-async function getPosts() {
-  let reddit = await fetch('https://api.pushshift.io/reddit/search/comment?q=budweiser&limit=50')
-  let resolvedReddit = await reddit.json()
-  // console.log(resolvedReddit.data);
-  let mappedReddit = resolvedReddit.data.map(data => ({
-    platform: 'Reddit',
-    user: data.author,
-    content: data.body,
-    source: data.permalink,
-    created_at: data.created_utc
-  })
-)
-console.log(mappedReddit);
 
 
+app.get("/results/:id", (req, res) => {
 
-  T.get('search/tweets', { q: 'iphone X -filter:retweets', lang: "en", count: 50 }, function(err, data, response) {
-     tweetdata = data.statuses.map(status => ({
-      platform: "Twitter",
-      user: status.user.name,
-      content: status.text,
-      source: "nill",
-      created_at: status.created_at
-    }))
-    // console.log(data);
-    console.log(JSON.stringify({tweetdata}, null, 2))
-  })
-}
+  async function getPosts() {
 
+    fetchRedditContent()
 
-
-    let posts = getPosts()
-    //
-    // res.send(computeData(posts))
+//   T.get('search/tweets', { q: 'iphone X -filter:retweets', lang: "en", count: 50 }, function(err, data, response) {
+//      tweetdata = data.statuses.map(status => ({
+//       platform: "Twitter",
+//       user: status.user.name,
+//       content: status.text,
+//       source: "nill",
+//       created_at: status.created_at
+//     }))
+//
+//     console.log(JSON.stringify({tweetdata}, null, 2))
+  // })
+  }
 })
-
-  // Make the express server serve static assets (html, javascript, css) from the /public folder
--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,27 @@ class App extends Component {
     }
   }
 
+  componentDidMount(){
+
+    this.callApi()
+      .then(res => {
+        this.setState({ response: res.express })
+        console.log(res);
+      })
+      .catch(err => console.log(err) );
+    }
+
+    callApi = async () => {
+      const response = await fetch('/api/hello');
+      // const body = await response.json();
+
+      // if (response.status !== 200) throw Error(body.message);
+      // console.log('yo')
+
+      // return body;
+    }
+
+
   searchSubmission = query => {
     this.setState({searchSubmit : true})
   }
