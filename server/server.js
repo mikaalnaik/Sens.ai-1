2a27b63511d35633cd311c7338c855623a054541
Hemantkumar Panchal <panchalhemant2001@yahoo.com>
2018-06-06 16:32:26 +0000

    commit after merging feature/dbmodules

--- server/server.js
+++ server/server.js
@@ -10,7 +10,6 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'))
-<<<<<<< HEAD
 .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
 //////////////// CODE PART - 1 : ROUTES FOR DATABASE MODULES - CREATED BY HEMANT /////////////////////////
@@ -36,21 +35,6 @@ app.get("/results/user/:id1/search/:id2", (req, res) => {
   })
 });
 //////////////////// END OF CODE PART - 1 /////////////////////////
-
-
-// Twitter API
-var T = new Twit({
-consumer_key: process.env.SECRETCONSUMERKEY,
-consumer_secret: process.env.SECRETCONSUMERSECRET,
-access_token: process.env.SECRETACCESSTOKEN,
-access_token_secret: process.env.SECRETACCESSTOKENSECRET,
-timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-strictSSL:            true,     // optional - requires SSL certificates to be valid.
-})
-console.log(T.config.consumer_key);
-=======
-  .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
->>>>>>> eeb956af6d14961da80aca19b890655d0af9dbb9
 
 app.get("/results/:id", async (req, res) => {
 
