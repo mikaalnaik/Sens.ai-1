19e6673235d01c288d63cd0b8d8063f35b06df8d
Cem Hekimoglu <cemhekimoglu.ch@gmail.com>
2018-06-08 12:01:25 -0400

    finalized the scroll feature

--- src/OverallSentiments.jsx
+++ src/OverallSentiments.jsx
@@ -10,6 +10,8 @@ import {
 import {Grid, Row, Col} from 'react-flexbox-grid';
 import ScrollableAnchor, { configureAnchors } from 'react-scrollable-anchor'
 
+configureAnchors({scrollDuration: 1000})
+
 
 class OverallSentiment extends Component {
   constructor(props) {
